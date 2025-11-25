2e12895badd8e234022a025c8933492f9bc337da
Zane2300 <alexrosell2003@gmail.com>
2025-11-25 03:19:53 +0100

    Move Astro project to root structure

---
+++ astro.config.mjs
@@ -0,0 +1,19 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import tailwindcss from '@tailwindcss/vite';
+
+import svelte from '@astrojs/svelte';
+
+// https://astro.build/config
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
+  integrations: [svelte()],
+
+  site: 'https://zane2300.github.io/portfolio-alex',
+  base: '/portfolio-alex',
+  outDir: './docs', 
+});
